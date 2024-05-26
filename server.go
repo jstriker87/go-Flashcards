d40750fa51bc186141a61320bf1fc4f363a5e4a1
James <j.cumming-bart@lancaster.ac.uk>
2024-05-26 01:17:49 +0100

    Refactored code, so that the option to select if the user is ok with the question or if it needs revision is now displayed after the answer is shown

--- server.go
+++ server.go
@@ -4,11 +4,20 @@ import (
 	"log"
 	"net/http"
 	"html/template"
+    "runtime"
+    "os/exec"
+
 )
 
 type Flashcards struct {
     Question string
     Answer   string
+}
+
+type ContactDetails struct {
+    Email   string
+    Subject string
+    Message string
 }
 var needsRevision = false
 var flashcardCount = 0
@@ -17,9 +26,8 @@ var flashcards = []Flashcards{
     {"What is the capital of Scotland", "Edinburgh"},
 }
 
-func flashcardResult (w http.ResponseWriter, r *http.Request) {
-    if flashcardCount < len(flashcards){
-        flashTemplate := template.Must(template.ParseFiles("results.html"))
+func showAnswer(w http.ResponseWriter, r *http.Request) {
+        flashTemplate := template.Must(template.ParseFiles("answer.html"))
         data := map[string]Flashcards{
             "Flashcard": flashcards[flashcardCount],
 
@@ -28,21 +36,11 @@ func flashcardResult (w http.ResponseWriter, r *http.Request) {
             log.Println("Error executing template:", err)
 
         }
-        if needsRevision {
+}
 
-            flashcards = append(flashcards[:flashcardCount], flashcards[flashcardCount+1:]...)
-            needsRevision = false
-
-        }else{
-
-            flashcardCount++
-        }
-
-        }else{
-            http.Redirect(w, r, "/end", http.StatusSeeOther)           
-            }
-        }
-func checkFlashcards (w http.ResponseWriter, r *http.Request) {
+func showQuestion(w http.ResponseWriter, r *http.Request) {
+        //fmt.Printf("The flashcard count is %d \n",len(flashcards))
+        //fmt.Printf("The flashcardCount  count is %d \n",flashcardCount)
         if flashcardCount < len(flashcards){
         flashTemplate := template.Must(template.ParseFiles("questions.html"))
         data := map[string]Flashcards{
@@ -66,21 +64,41 @@ func startFlashcards (w http.ResponseWriter, r *http.Request) {
             log.Println("Error executing template:", err)
         }
     }
+func addQuestions(w http.ResponseWriter, r *http.Request) {
 
-func updateFlashcards (w http.ResponseWriter, r *http.Request) {
-    needsRevision = true
-    http.Redirect(w, r, "/results", http.StatusSeeOther)
+        flashTemplate := template.Must(template.ParseFiles("add.html"))
+        data := map[string]int{
+            "Flashcard": 0,
+        }
+        if err := flashTemplate.Execute(w, data); err != nil {
+            log.Println("Error executing template:", err)
+        }
+    }
+
+
+func questionNeedsRevision (w http.ResponseWriter, r *http.Request) {
+    flashcardCount++
+    http.Redirect(w, r, "/question", http.StatusSeeOther)
 
 }
-func restartFlashcards (w http.ResponseWriter, r *http.Request) {
-    needsRevision = false 
-    flashcardCount = 0
-    http.Redirect(w, r, "/start", http.StatusSeeOther)
+
+
+func questionOK (w http.ResponseWriter, r *http.Request) {
+    flashcards = append(flashcards[:flashcardCount], flashcards[flashcardCount+1:]...) 
+    flashcardCount++
+    http.Redirect(w, r, "/question", http.StatusSeeOther)
+
+}
+
+
+func restart (w http.ResponseWriter, r *http.Request) {
+    flashcardCount=0
+    http.Redirect(w, r, "/question", http.StatusSeeOther)
 
 }
 
 func endFlashcards (w http.ResponseWriter, r *http.Request) {
-    if flashcardCount == 0{
+    if len(flashcards) == 0{
         http.Redirect(w, r, "/", http.StatusSeeOther)
 
 
@@ -95,15 +113,47 @@ func endFlashcards (w http.ResponseWriter, r *http.Request) {
         }
     }
 
+func addPartTwo(w http.ResponseWriter, r *http.Request) {
+    if r.Method == "GET" {
+        t, _ := template.ParseFiles("add.html")
+        t.Execute(w, nil)
+    } else {
+        //result:= r.ParseForm()
+        //newFlashcard := Flashcards{result,result}
+        //flashcards = append(flashcards, newFlashcard)
+//        fmt.Println(flashcards)
+    }
+}
+
+func openServerWebpage(url string) error {
+    var cmd string
+    var args []string
+    switch runtime.GOOS {
+    case "windows":
+        cmd = "cmd"
+        args = []string{"/c", "start"}
+    case "darwin":
+        cmd = "open"
+    default: // "linux", "freebsd", "openbsd", "netbsd"
+        cmd = "xdg-open"
+    }
+    args = append(args, url)
+    return exec.Command(cmd, args...).Start()
+}
+
 func main() {
 
+    openServerWebpage("http://localhost:8000")
     fileServer := http.FileServer(http.Dir("./static"))
     http.Handle("/static/", http.StripPrefix("/static/", fileServer))
     http.HandleFunc("/", startFlashcards)
-    http.HandleFunc("/start", checkFlashcards)
-    http.HandleFunc("/update", updateFlashcards)
-    http.HandleFunc("/results", flashcardResult)
+    http.HandleFunc("/show",addPartTwo) 
+    http.HandleFunc("/question", showQuestion)
+    http.HandleFunc("/add", addQuestions)
+    http.HandleFunc("/needsRevision", questionNeedsRevision)
+    http.HandleFunc("/ok", questionOK)
+    http.HandleFunc("/answer", showAnswer)
+    http.HandleFunc("/restart", restart)
     http.HandleFunc("/end", endFlashcards)
-    http.HandleFunc("/restart", restartFlashcards)
     log.Fatal(http.ListenAndServe(":8000", nil))
 }
