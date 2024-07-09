7e854f2c9601a7361d7545194d3ea8f9386040e4
James Bart <jbart1@gmail.com>
2024-07-09 18:01:17 +0100

    Still working on getting sound toggling working correctly

--- static/js/script.js
+++ static/js/script.js
@@ -7,3 +7,48 @@ function goPlay(musicurl,url){
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
+function volume() {
+
+    console.log("The Start volume when vol function run is " + localStorage.getItem("toggleVolume"));
+    if (localStorage.getItem("toggleVolume") == "false") {
+        localStorage.setItem("toggleVolume", "true");
+        document.getElementById("volume-icon").src = "/static/images/volume-on.svg";
+        updateVolume(true);
+        return;
+    } else {
+        localStorage.setItem("toggleVolume", "false");
+        updateVolume(false);
+        return;
+
+    }
+}
+
+function updateVolume(isMuted){
+    
+    var okButton = document.getElementsByClassName("ok-button");
+    var needsRevisonButton = document.getElementsByClassName("revision-button");
+      
+    if (isMuted==true){
+        okButton.muted = true;
+        needsRevisonButton.muted = true;
+        document.getElementById("volume-icon").src = "/static/images/volume-on.svg";
+
+    } 
+
+    if (isMuted==false){
+        okButton.muted = false;
+        needsRevisonButton.muted = false;
+        document.getElementById("volume-icon").src = "/static/images/volume-off.svg";
+    }
+
+}
+
+function volumeOnLoad(){
+    console.log("The volume setting on load is " + localStorage.getItem("toggleVolume"));
+    if (localStorage.getItem("toggleVolume") == "true") {
+        document.getElementById("volume-icon").src = "/static/images/volume-on.svg";
+    }else{
+        
+        document.getElementById("volume-icon").src = "/static/images/volume-off.svg";
+    }
+}
