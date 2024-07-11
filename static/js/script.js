abd3213e701296daed35a5ca911bb3e5d6dd2be8
James Bart <jbart1@gmail.com>
2024-07-11 21:18:12 +0100

    Volume option now works. Sounds are currently only for choosing if the answer is ok or if it needs revision

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,8 @@
 function goPlay(musicurl,url){
-  var music = new Audio(musicurl);
-  music.play();
+  if (localStorage.getItem("toggleVolume") == "true") {
+    var music = new Audio(musicurl);
+    music.play();
+  }
   sleep(1500).then(() => { window.location.href = url; }); 
   }
 
@@ -8,48 +10,25 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 function volume() {
-
     if (localStorage.getItem("toggleVolume") == "false") {
         localStorage.setItem("toggleVolume", "true");
         document.getElementById("volume-icon").src = "/static/images/volume-on.svg";
-        updateVolume(true);
-        console.log("The volume setting when vol function run is " + localStorage.getItem("toggleVolume"));
         return;
     } else {
         localStorage.setItem("toggleVolume", "false");
-        updateVolume(false);
-        console.log("The volume setting when vol function run is " + localStorage.getItem("toggleVolume"));
+        document.getElementById("volume-icon").src = "/static/images/volume-off.svg";
         return;
 
     }
 }
 
-function updateVolume(isMuted){
-    
-    var okButton = document.getElementsByClassName("ok-button");
-    var needsRevisonButton = document.getElementsByClassName("revision-button");
-      
-    if (isMuted==false){
-        okButton.muted = false;
-        needsRevisonButton.muted = false;
-        document.getElementById("volume-icon").src = "/static/images/volume-off.svg";
-
-    } 
-
-    if (isMuted==true){
-        okButton.muted =true;
-        needsRevisonButton.muted =true;
-        document.getElementById("volume-icon").src = "/static/images/volume-on.svg";
-    }
-
-}
-
 function volumeOnLoad(){
-    console.log("The volume setting on load is " + localStorage.getItem("toggleVolume"));
+    if (localStorage.getItem("toggleVolume") == null) {
+        localStorage.setItem("toggleVolume", "false");
+    }
     if (localStorage.getItem("toggleVolume") == "true") {
-        updateVolume(true)
+        document.getElementById("volume-icon").src = "/static/images/volume-on.svg";
     }else{
-        
-        updateVolume(false)
+        document.getElementById("volume-icon").src = "/static/images/volume-off.svg";
     }
 }
